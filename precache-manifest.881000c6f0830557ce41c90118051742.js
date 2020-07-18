36056391a0cfaf3c6a16e34536821dc02b52b564
Nirjhar Debnath <nirjhar@Nirjhars-MacBook-Air.local>
2020-07-18 16:27:52 +0530

    Updates

---
+++ precache-manifest.881000c6f0830557ce41c90118051742.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0ac1f50b03710b3c56ce0117d83145",
+    "url": "/Teachzy-Feature-Discovery/index.html"
+  },
+  {
+    "revision": "29bb07e7c5bb72af354e",
+    "url": "/Teachzy-Feature-Discovery/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "40275c2f74d5ba2d72ff",
+    "url": "/Teachzy-Feature-Discovery/static/js/2.073ee404.chunk.js"
+  },
+  {
+    "revision": "7979eeaf519f92e9fad084e05c681841",
+    "url": "/Teachzy-Feature-Discovery/static/js/2.073ee404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29bb07e7c5bb72af354e",
+    "url": "/Teachzy-Feature-Discovery/static/js/main.8f9072d1.chunk.js"
+  },
+  {
+    "revision": "33f98f40aeebc3759857",
+    "url": "/Teachzy-Feature-Discovery/static/js/runtime-main.6725b4fc.js"
+  },
+  {
+    "revision": "0eb0e988707a2450e13415779ae41410",
+    "url": "/Teachzy-Feature-Discovery/static/media/createDeleteNotes.0eb0e988.gif"
+  },
+  {
+    "revision": "c1382f6f2bde2b2103c7fd0e77bd3d93",
+    "url": "/Teachzy-Feature-Discovery/static/media/createGroup.c1382f6f.gif"
+  },
+  {
+    "revision": "183bce94c577e284ab1a2a7247c49a7c",
+    "url": "/Teachzy-Feature-Discovery/static/media/deleteGroup.183bce94.gif"
+  },
+  {
+    "revision": "82581dac060ec2af00e23e92c17ad6b4",
+    "url": "/Teachzy-Feature-Discovery/static/media/events.82581dac.gif"
+  },
+  {
+    "revision": "bef83ae17744b292ff2c5a7c8e0591a8",
+    "url": "/Teachzy-Feature-Discovery/static/media/feePayments.bef83ae1.gif"
+  },
+  {
+    "revision": "5c54ff95ce50868f521afa7f188792b4",
+    "url": "/Teachzy-Feature-Discovery/static/media/fees.5c54ff95.gif"
+  },
+  {
+    "revision": "9f9b976cc5ff1192d9b546a49246031f",
+    "url": "/Teachzy-Feature-Discovery/static/media/markAttendance.9f9b976c.gif"
+  },
+  {
+    "revision": "6202d7d09df803fdfc22b422f7d4f9d1",
+    "url": "/Teachzy-Feature-Discovery/static/media/removeStudent.6202d7d0.gif"
+  },
+  {
+    "revision": "a5ac14b63f0584e9d6026f94392a8c97",
+    "url": "/Teachzy-Feature-Discovery/static/media/shareGroup.a5ac14b6.gif"
+  },
+  {
+    "revision": "3c3aa6d4d17dedb9cb2975636b52cb66",
+    "url": "/Teachzy-Feature-Discovery/static/media/studentDetails.3c3aa6d4.gif"
+  },
+  {
+    "revision": "836d5121bd9183e8fa3e525804a28b6c",
+    "url": "/Teachzy-Feature-Discovery/static/media/studentSubmission.836d5121.gif"
+  },
+  {
+    "revision": "c1297805c804dd00ea7ea85c0695364a",
+    "url": "/Teachzy-Feature-Discovery/static/media/useFeed.c1297805.gif"
+  }
+]);
